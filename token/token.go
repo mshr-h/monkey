55cecf08a3b5a3a5af4482407c3d35e5aebd7d51
mshr-h <mshr-h@users.noreply.github.com>
2018-07-01 22:38:57 +0900

    add definition of token types

---
+++ token/token.go
@@ -0,0 +1,34 @@
+// token/token.go
+
+package token
+
+// TokenType represents type of token
+type TokenType string
+
+// Token represents Token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 132134
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
