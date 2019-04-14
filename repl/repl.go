44edafea350d749bcd318794304b721675566f2f
mshr-h <mshr-h@users.noreply.github.com>
2019-04-14 11:22:32 +0900

    print parser result

--- repl/repl.go
+++ repl/repl.go
@@ -6,12 +6,12 @@ import (
 	"io"
 
 	"github.com/mshr-h/monkey/lexer"
-	"github.com/mshr-h/monkey/token"
+	"github.com/mshr-h/monkey/parser"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Reader) {
+func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
@@ -23,9 +23,22 @@ func Start(in io.Reader, out io.Reader) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
